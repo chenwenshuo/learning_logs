5e3d8548709f77213ceb10e517339509d05eac30
陈文硕 <362535136@qq.com>
2019-09-11 20:47:30 +0800

    django

---
+++ learning_log/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.text
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text = models.TextField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+# s使用entries 管理条目
+    class Meta:
+        verbose_name_plural = 'entries'
+    def __str__(self):
+        return self.text[:50]
